21984b8e6ca8e07d3f3ece5d4cc88172379bf97a
ZachSatt <64815987+ZachSatt@users.noreply.github.com>
2020-10-21 20:43:24 -0700

    Polygon shading complete - error over wrong city

--- client/src/component/Map.jsx
+++ client/src/component/Map.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { GoogleMap, useLoadScript, Marker, InfoWindow, } from "@react-google-maps/api";
+//import { useLoadScript, withScriptjs, withGoogleMap, Marker, InfoWindow, GoogleMap, Polygon } from "react-google-maps";
+import { GoogleMap, DrawingManager, useLoadScript, Marker, InfoWindow, Polygon } from "@react-google-maps/api";
 import usePlacesAutocomplete, { getGeocode, getLatLng, } from "use-places-autocomplete";
 import { Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption, } from "@reach/combobox";
 import { formatRelative } from "date-fns";
@@ -20,6 +21,38 @@ const center = {
   lat: 34.048927,
   lng: -111.093735,
 };
+
+//Pre-Blackout Coordinates
+const coords = [
+  { lat: 32.214176, lng: -110.926567 },
+  { lat: 32.207069, lng: -110.926557 },
+  { lat: 32.207187, lng: -110.924571 },
+  { lat: 32.207227, lng: -110.922353 },
+  { lat: 32.207200, lng: -110.920344 },
+  { lat: 32.207187, lng: -110.918106 },
+  { lat: 32.207177, lng: -110.916305 },
+  { lat: 32.207182, lng: -110.913555 },
+  { lat: 32.207209, lng: -110.912443 },
+  { lat: 32.207486, lng: -110.912448 },
+  { lat: 32.207590, lng: -110.912234 },
+  { lat: 32.207659, lng: -110.912078 },
+  { lat: 32.208217, lng: -110.912180 },
+  { lat: 32.208870, lng: -110.911746 },
+  { lat: 32.209161, lng: -110.911515 },
+  { lat: 32.209252, lng: -110.911348 },
+  { lat: 32.209379, lng: -110.911144 },
+  { lat: 32.209270, lng: -110.910865 },
+  { lat: 32.209247, lng: -110.909829 },
+  { lat: 32.216051, lng: -110.909743 },
+  { lat: 32.221297, lng: -110.909789 },
+  { lat: 32.221424, lng: -110.917988 },
+  { lat: 32.214312, lng: -110.918086 },
+  { lat: 32.214181, lng: -110.922248 },
+  { lat: 32.214176, lng: -110.926567 }];
+
+const blackout = coords.map( ll => {
+  return { lat: ll.lng, lng: ll.lat }
+});
 
 export default function App() {
   const { isLoaded, loadError } = useLoadScript({
@@ -79,13 +112,25 @@ export default function App() {
               setSelected(marker);
             }}
             icon={{
-              url: `/maplock.png`,
+              url: `./maplock.png`,
               origin: new window.google.maps.Point(30, 30),
               anchor: new window.google.maps.Point(15, 15),
               scaledSize: new window.google.maps.Size(30, 30),
             }}
           />
         ))}
+
+        <Polygon
+            path={blackout}
+            //key={1}
+            options={{
+                fillColor: "#000",
+                fillOpacity: 0.4,
+                strokeColor: "#000",
+                strokeOpacity: 1,
+                strokeWeight: 1
+            }} 
+        />
 
         {selected ? (
           <InfoWindow
