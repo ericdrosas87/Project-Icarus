5d821cd7475a6b7d5b7dfdcc5d5cdf7a7c9ada15
Jrios <Jesusfernandorios93@gmail.com>
2020-10-28 14:40:59 -0700

    setting the stage for development

--- client/src/component/Product.jsx
+++ client/src/component/Product.jsx
@@ -53,30 +53,6 @@ function Product({ name, image }) {
                 </div>
                 
             </div>
-
-
-  return (
-    <div className="product">
-      <div className="img">
-        <img className="product__image" src={image} alt="Daedelus" />
-      </div>
-      <div className="product__info">
-        <h2>{name}</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id,
-          molestiae? Tempore quis veritatis tempora ullam quia alias placeat
-          repellat quasi pariatur fuga provident, dolorem, ratione nobis
-          impedit, eius nulla sint?
-        </p>
-      <div className="product__buttons">
-        <button onClick={(e) => preOrder(e)}>Add</button>
-        <Link to="/contact">
-          <button className="product__buttons" href="/contact">
-            Contact Us
-          </button>
-        </Link>
-          </div>
-        </div>
         
             <>
                 <Modal show={modalTwo} onHide={handleCloseModalTwo}>
