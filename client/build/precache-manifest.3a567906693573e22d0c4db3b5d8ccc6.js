33a2cb950e2b08d4b8ae169a59b10a7a7532ebc7
Jrios <Jesusfernandorios93@gmail.com>
2020-10-30 15:14:35 -0700

    ran npm build and manually deployed

---
+++ client/build/precache-manifest.3a567906693573e22d0c4db3b5d8ccc6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd75ae23372ea3163007",
+    "url": "/static/css/main.0e6fbb3e.chunk.css"
+  },
+  {
+    "revision": "bd75ae23372ea3163007",
+    "url": "/static/js/main.8ac82358.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c192e98b068585883177",
+    "url": "/static/css/2.7c87403a.chunk.css"
+  },
+  {
+    "revision": "c192e98b068585883177",
+    "url": "/static/js/2.c89e81f7.chunk.js"
+  },
+  {
+    "revision": "359576224b883c0c5defacf388b719b2",
+    "url": "/static/media/360view.35957622.jpg"
+  },
+  {
+    "revision": "3868cd9a58182fc0482316d9e18b170f",
+    "url": "/static/media/360close.3868cd9a.jpg"
+  },
+  {
+    "revision": "6ac706490a590f558733d889abfa600b",
+    "url": "/static/media/360top.6ac70649.jpg"
+  },
+  {
+    "revision": "5e16442531d44ab5d2e966a2cafa2426",
+    "url": "/static/media/360side.5e164425.jpg"
+  },
+  {
+    "revision": "2c9e2514af9cfd43b24bf54b0bbbcc8e",
+    "url": "/static/media/landing-phoenix.2c9e2514.JPG"
+  },
+  {
+    "revision": "b8682f8c3d0a9eeb898f20c81cf1d8f3",
+    "url": "/static/media/landing-tucson.b8682f8c.JPG"
+  },
+  {
+    "revision": "233d86ff3428151e33bd5dcf8439ff4e",
+    "url": "/index.html"
+  }
+];
